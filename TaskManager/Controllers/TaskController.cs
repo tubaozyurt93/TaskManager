d5af3d240f2a85f9adc18f07e32576e0b99b2f25
Tuba Özyurt <tuba.ozyurt93@gmail.com>
2021-08-01 19:06:43 +0300

    Görev Yöneticisi
    AspNet Core Görev Yöneticisi

---
+++ TaskManager/Controllers/TaskController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly TaskContext _context;
+
+        public TaskController(TaskContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Works.ToListAsync());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Create([Bind("Id,Name,EnterDate,TaskType,TaskDuration")] Work work)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(work);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(work);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var work = await _context.Works.FindAsync(id);
+            if (work == null)
+            {
+                return NotFound();
+            }
+            return View(work);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,EnterDate,JobType,JobDuration")] Work work)
+        {
+            if (id != work.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(work);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WorkExists(work.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(work);
+        }
+        private bool WorkExists(int id)
+        {
+            return _context.Works.Any(e => e.Id == id);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var work = await _context.Works
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (work == null)
+            {
+                return NotFound();
+            }
+
+            return View(work);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var work = await _context.Works.FindAsync(id);
+            _context.Works.Remove(work);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ TaskManager/Models/Work.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models
+{
+    public class Work
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime EnterDate { get; set; }
+        public string TaskType { get; set; }
+        public string TaskDuration { get; set; }
+    }
+}
